2bcda3225268325b2b64b4cffffcaea53399b446
Tyler Kellen <tyler@sleekcode.net>
2012-10-03 14:51:34 -0500

    testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,13 +31,22 @@ module.exports = function(grunt) {
         node: true,
         es5: true
       }
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js']
     }
   });
 
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', 'nodeunit');
+
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'test']);
 
 };
